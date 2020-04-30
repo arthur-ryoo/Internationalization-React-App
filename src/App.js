ac3d9f1f0269f485b32a5cbb75d6f1bc10a6e213
arthur-ryoo <jene0523@gmail.com>
2020-04-30 00:23:29 -0700

    Add translation functionality

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { useTranslation } from 'react-i18next';
 
 function App() {
+  const { t, i18n } = useTranslation();
+
+  function handleClick(lang) {
+    i18n.changeLanguage(lang);
+  }
+
   return (
     <div className="App">
+      <nav style={{ width: '100%', padding: '2rem 0', backgroundColor: 'gray' }}>
+        <button onClick={() => handleClick('en')}>English</button>
+        <button onClick={() => handleClick('ko')}>Korean</button>
+      </nav>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>{t('React.1')}</p>
+        <p>{t('App.1')}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
